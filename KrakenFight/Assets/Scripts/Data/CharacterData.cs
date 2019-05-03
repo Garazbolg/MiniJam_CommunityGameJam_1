e894039e4da21a6976a1535dff414f7e268023a3
Garazbolg <legall.joseph@laposte.net>
2019-05-03 07:47:52 +0200

    Basic data classes
    Those classes should be commented to describe how they work and should be used. But for now just check out this video about the way this variable classes should be used : https://youtu.be/raQ3iHhE_Kk?t=1061

---
+++ KrakenFight/Assets/Scripts/Data/CharacterData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Data.Variables;
+
+namespace Game.Data
+{
+	[CreateAssetMenu]
+	public class CharacterData : ScriptableObject
+	{
+		public SliderVariable HP;
+		public FloatVariable Speed;
+		public FloatVariable Damage;
+	}
+
+}
---
+++ KrakenFight/Assets/Scripts/Data/Helpers/SliderSetter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Data.Variables;
+
+
+[ExecuteInEditMode]
+public class SliderSetter : MonoBehaviour
+{
+	public Slider Slider;
+	public FloatVariable Variable;
+
+	private void Update()
+	{
+		if (Slider != null && Variable != null)
+			Slider.value = Variable.Value;
+	}
+}
