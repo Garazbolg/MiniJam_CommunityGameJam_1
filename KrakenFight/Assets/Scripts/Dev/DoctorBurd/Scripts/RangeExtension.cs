2f4ec92fe68e73f4a7273b093d34ce2bb58154fe
Jonathan Burd <mail@jonathanburd.com>
2019-05-07 03:05:14 -0500

    KrakenAggro class
    Returns string description of Kraken's aggressiveness and allows other functions to alter  the current aggro level  (due to damage, quick movements, etc.)

---
+++ KrakenFight/Assets/Scripts/Dev/DoctorBurd/Scripts/RangeExtension.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class RangeExtension
+{
+    public static bool Between(this int value,int min,int max)
+    {
+        return value >= min && value <= max;
+    }
+}
