ac08d81fcc76f1d338c112700765ca4dc0ab1113
Jonathan Burd <mail@jonathanburd.com>
2019-05-07 00:04:43 -0500

    Initial In-Progress push to new fork

---
+++ KrakenFight/Assets/Scripts/Dev/DoctorBurd/Scripts/KrakenAggro.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KrakenAggro : MonoBehaviour
+{
+    // How aggressive if the Kraken
+    public static int krakenCurrentAggro    = 0;
+    // String 
+    public static string krakenAggroLevel   = "Unknown";
+
+
+
+    public void ChangeKrakenAggro()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ KrakenFight/Assets/Scripts/Dev/DoctorBurd/Scripts/Resources.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    [System.Serializable]
+
+    [CreateAssetMenu(fileName = "New Resource",menuName = "Resource Item",order = 51)]
+    public class Resources : ScriptableObject
+    {
+        [SerializeField]
+        public string resourceName;
+        [SerializeField]
+        public int resourceBuyCost;
+        [SerializeField]
+        public int resourceSellCost;
+        [SerializeField]
+        public int maxInventoryStack;
+
+        [SerializeField]
+        public Sprite resourceIcon;
+    }
