87d98c67f6a32a34db9836caf2f147703ec99c5f
Jonathan Burd <mail@jonathanburd.com>
2019-05-08 22:12:43 -0500

    Updated scripts.
    per AmusedSandpaper's code review

--- KrakenFight/Assets/Scripts/Data/KrakenAggro.cs
+++ KrakenFight/Assets/Scripts/Data/KrakenAggro.cs
@@ -17,34 +17,50 @@ public class KrakenAggro : MonoBehaviour
     // How aggressive is the Kraken; can be changed by altering  int KrakenAggro.changeToKrakenAggro
     public static int krakenCurrentAggro    = 0;
     // string name from enum for different levels of Aggressiveness
-    AggroStates krakenAggroLevel = (AggroStates)0;
+    AggroStates krakenAggroLevel = AggroStates.Unseen;
     
-    // Change in value to apply to krakenCurrentAggro during Update() - to be used by outside classes?
-    public int changeToKrakenAggro          = 0;
-
- 
     // returns string name for different levels of Aggressiveness
-    public void CheckKrakenAggroLevel()
+    public void SetKrakenAggroLevel()
     {
-        
+        if (RangeExtension.Between(krakenCurrentAggro,0,9))
+        {
+            krakenAggroLevel = AggroStates.Unseen; // HUD for Kraken Aggro - blank
+        }        
+        else if (RangeExtension.Between(krakenCurrentAggro,10,19))
+        {
+            krakenAggroLevel = AggroStates.Nearby; // HUD for Kraken Aggro - 'Shadow'
+        }      
+        else if (RangeExtension.Between(krakenCurrentAggro,20,39))
+        {
+            krakenAggroLevel = AggroStates.Curious; // HUD for Kraken Aggro - Green & "peeking out" (i.e. top half)
+        }     
+        else if (RangeExtension.Between(krakenCurrentAggro,40,59))
+        {
+            krakenAggroLevel = AggroStates.Enraged; // HUD for Kraken Aggro - Green & fully visible
+        }     
+        else if (RangeExtension.Between(krakenCurrentAggro,60,89))
+        {
+            krakenAggroLevel = AggroStates.Frenzied; // HUD for Kraken Aggro - Orange
+        }    
+        else if (RangeExtension.Between(krakenCurrentAggro,90,100))
+        {
+            krakenAggroLevel = AggroStates.Wrathful; // HUD for Kraken Aggro - Red
+        }
 
-        if (RangeExtension.Between(krakenCurrentAggro,0,9) == true) { krakenAggroLevel = (AggroStates)0; }        // HUD for Kraken Aggro - blank
-        if (RangeExtension.Between(krakenCurrentAggro,10,19) == true) { krakenAggroLevel = (AggroStates)1; }      // HUD for Kraken Aggro - 'Shadow'
-        if (RangeExtension.Between(krakenCurrentAggro,20,39) == true) { krakenAggroLevel = (AggroStates)2; }     // HUD for Kraken Aggro - Green & "peeking out" (i.e. top half)
-        if (RangeExtension.Between(krakenCurrentAggro,40,59) == true) { krakenAggroLevel = (AggroStates)3; }     // HUD for Kraken Aggro - Green & fully visible
-        if (RangeExtension.Between(krakenCurrentAggro,60,89) == true) { krakenAggroLevel = (AggroStates)4; }    // HUD for Kraken Aggro - Orange
-        if (RangeExtension.Between(krakenCurrentAggro,90,100) == true) { krakenAggroLevel = (AggroStates)5; }   // HUD for Kraken Aggro - Red
+        Debug.Log("Kraken Aggro State = " + krakenAggroLevel);
+    }
 
-        Debug.Log(krakenAggroLevel);
+
+    // pass an Int value to this function to change the Kraken's Aggro
+    public void ChangeKrakenAggro(int amount)
+    {
+        krakenCurrentAggro += amount;
+        krakenCurrentAggro = Mathf.Clamp(krakenCurrentAggro,0,100);
     }
 
     void Update()
     {
-        krakenCurrentAggro += changeToKrakenAggro;
-        krakenCurrentAggro = Mathf.Clamp(krakenCurrentAggro,0,100);
-
-        Debug.Log(krakenCurrentAggro);
-        CheckKrakenAggroLevel();
-        Debug.Log(krakenAggroLevel);
+        ChangeKrakenAggro(0);
+        SetKrakenAggroLevel();
     }
 }
--- KrakenFight/Assets/Scripts/Data/Resources.cs
+++ KrakenFight/Assets/Scripts/Data/Resources.cs
@@ -7,15 +7,10 @@ using UnityEngine;
     [CreateAssetMenu(fileName = "New Resource",menuName = "Resource Item",order = 51)]
     public class Resources : ScriptableObject
     {
-        [SerializeField]
         public string resourceName;
-        [SerializeField]
         public int resourceBuyCost;
-        [SerializeField]
         public int resourceSellCost;
-        [SerializeField]
         public int maxInventoryStack;
 
-        [SerializeField]
         public Sprite resourceIcon;
     }
